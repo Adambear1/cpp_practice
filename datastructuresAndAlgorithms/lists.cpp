8ae30592fcf9332df8cb8d4a5352087b7f06b525
Adam Birgenheier <adamcarlbirgenheier@gmail.com>
2021-06-09 12:03:17 -0400

    init dataStructure

---
+++ datastructuresAndAlgorithms/lists.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+// LISTS:
+//  -   Stores a given number of elements of a given data-type.
+//      -   Able to declare in array parameter.
+//  -   Declare what type(s) of data are being passed.
+//  -   Write/Modify element at position.
+//  -   Read element at a position.
+//  -   Abstract data type as is (for reasons as mentioned), because it merely molds
+//      or shapes the data as intended, but doesn't perform functionality (30,000ft view)
+//      -   Access to read/write data requires const time.
+//          -   If last index of data manipulated, then O(1) time taken, because no other data is affected.
+//          -   If any other index of data is manipulated, then O(x) time taken, where x is number of other
+//              items in array/list affected
+int main()
+{
+    return 0;
+}
